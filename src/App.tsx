f0e580d9cfff46a9757219826ca06e92801a2acc
guntza10 <jomphop.saibuatong@hotmail.com>
2025-04-19 15:04:48 +0700

    feat(Navbar): initiate Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import "./App.css";
-
 import { useState } from "react";
 
 import LoadingScreen from "./components/LoadingScreen";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
 
   const handleCompleted = () => setIsLoaded(true);
 
-  return <>{!isLoaded && <LoadingScreen onCompleted={handleCompleted} />}</>;
+  return (
+    <>
+      {!isLoaded && <LoadingScreen onCompleted={handleCompleted} />}
+      <div
+        className={`min-h-screen bg-black text-gray-700 ${isLoaded ? "opacity-100" : "opacity-0"} transition-opacity duration-700`}
+      >
+        <Navbar />
+      </div>
+    </>
+  );
 }
 
 export default App;
