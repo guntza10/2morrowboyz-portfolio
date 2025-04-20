90fca44637ae5276b3f2bd57484d969e94f7e57f
guntza10 <jomphop.saibuatong@hotmail.com>
2025-04-20 17:35:30 +0700

    refactor(Navbar): implement Navbar done

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,13 @@ import LoadingScreen from "./components/LoadingScreen";
 import Navbar from "./components/Navbar";
 
 function App() {
+  // isLoaded
   const [isLoaded, setIsLoaded] = useState(false);
 
   const handleCompleted = () => setIsLoaded(true);
+
+  // menuOpen
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <>
@@ -14,7 +18,7 @@ function App() {
       <div
         className={`min-h-screen bg-black text-gray-700 ${isLoaded ? "opacity-100" : "opacity-0"} transition-opacity duration-700`}
       >
-        <Navbar />
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       </div>
     </>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const Navbar: React.FC = () => {
-  return <nav>Navbar</nav>;
+interface NavbarProps {
+  menuOpen: boolean;
+  setMenuOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ menuOpen, setMenuOpen }) => {
+  useEffect(() => {
+    // Prevent scrolling when the menu is open (for mobile)
+    // This is a workaround for the issue where the body scrolls when the menu is open
+    // and the menu is positioned fixed
+    // This is not the best solution, but it works for now
+    // You can also use a library like react-scroll-lock or react-modal to handle this
+    document.body.style.overflow = menuOpen ? "hidden" : "";
+  }, [menuOpen]);
+
+  return (
+    <nav className="bg-navbar fixed top-0 left-0 z-40 w-full border-b border-white/10 shadow-lg backdrop-blur-lg">
+      <div className="mx-auto max-w-5xl px-4">
+        <div className="flex h-16 items-center justify-between">
+          <a href="#home" className="font-mono text-xl font-bold text-white">
+            Jomphop <span className="text-blue-500">Saibuatong</span>
+          </a>
+
+          <div
+            className="relative z-40 h-5 w-7 cursor-pointer md:hidden"
+            onClick={() => setMenuOpen((prev) => !prev)}
+          >
+            &#9776;
+          </div>
+
+          <div className="hidden items-center gap-8 md:flex">
+            <a href="#home" className="nav-link">
+              Home
+            </a>
+            <a href="#about" className="nav-link">
+              About
+            </a>
+            <a href="#projects" className="nav-link">
+              Projects
+            </a>
+            <a href="#contact" className="nav-link">
+              Contact
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
-export default Navbar;
+export default React.memo(Navbar);
