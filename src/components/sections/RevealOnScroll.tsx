75c20e61ccb1ee0fc3891de8cd8f713a8de4eed2
guntza10 <jomphop.saibuatong@hotmail.com>
2025-06-08 17:26:53 +0700

    refactor(reveal on scroll): adjust translate y 40

--- src/components/sections/RevealOnScroll.tsx
+++ src/components/sections/RevealOnScroll.tsx
@@ -8,7 +8,7 @@ const RevealOnScroll = ({ children }: { children: React.ReactNode }) => {
     <div
       ref={ref}
       className={`transform transition-all duration-1000 ease-in ${
-        isInView ? "translate-y-0 opacity-100" : "translate-y-10 opacity-0"
+        isInView ? "translate-y-0 opacity-100" : "translate-y-40 opacity-0"
       }`}
     >
       {children}
